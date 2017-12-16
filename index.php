53dcb1fb7b0ea31ef68261876ecb59274d46349a
Shivank-Lakhera <shivanklakhera@gmail.com>
2017-12-16 17:30:52 +0530

    calculator is done

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
 <?php
-    echo "calculator";
+
+if(isset($_POST['sub']))
+{
+	$txt1=$_POST['n1'];
+	$txt2=$_POST['n2'];
+	$oprnd=$_POST['sub'];
+	
+	if($oprnd=="+")
+    {
+    $res=$txt1+$txt2;
+        echo: res;
+    }
+	
+	
+    else if($oprnd=="-")
+		 {
+    $res=$txt1-$txt2;
+        echo: res;
+    }
+		
+    else if($oprnd=="*")
+		 {
+    $res=$txt1*$txt2;
+        echo: res;
+    }
+		
+    else if($oprnd=="/")
+    {
+        $res=$txt1/$txt2;
+        echo: res;
+    }
+}
 ?>
+<html>
+    <body>
+      <form method="post" action="">
+
+      <br>
+      <input name="n1" value="<?php echo $txt1; ?>">
+      <br>
+      <input name="n2" value="<?php echo $txt2; ?>">
+      <br>
+          <input name="res" value="<?php echo $res; ?>">
+          <br>
+          <input type="submit" name="sub" value="+">
+          <input type="submit" name="sub" value="-">
+          <input type="submit" name="sub" value="*">
+          <input type="submit" name="sub" value="/">
+        </form>
+    </body>
+</html>
